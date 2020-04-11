5a5bda47901abec205ee68ce36276ad048bc23da
Dianna Tingg <dtingg@gmail.com>
2020-04-11 14:03:41 -0700

    added time and space

--- lib/dijkstra.rb
+++ lib/dijkstra.rb
@@ -1,3 +1,4 @@
+# Find the unvisited node with the shortest distance
 def find_next_node(added, shortest_distances)
   distance = Float::INFINITY
   next_node = nil
@@ -12,32 +13,30 @@ def find_next_node(added, shortest_distances)
   return next_node
 end
 
+# Time: O(n^2 * e)
+# Space: O(n)
 def dijkstra(adjacency_matrix, start_node) 
-  #int nVertices = adjacencyMatrix[0].length; 
   num_nodes = adjacency_matrix.length
 
-  # shortest_distances will hold the shortest distances from start_node to i
-  # it starts with infinity as the value
+  # Holds the shortest distances from start_node to node. Each one starts with infinity.
   shortest_distances = Array.new(num_nodes, Float::INFINITY)
 
-  # Create a list of nodes called added which will record if a path to that node has already been discovered and set every entry to false
+  # Records if a path to that node has already been discovered. Each one starts as false.
   added = Array.new(num_nodes, false)
 
-  # Create an array called parents which will record the previous of any particular node on the path from the origin.
+  # Holds the previous node for each node. Each one starts as nil.
   parent_list = Array.new(num_nodes)
 
-  # Assign shortest_distances[origin_node] to 0, so that it is picked first
+  # Set the shortest_distance for start_node to 0 so it is picked first.
   shortest_distances[start_node] = 0
 
   answer = { start_node: start_node, parent_list: parent_list, shortest_distances: shortest_distances }
 
-
   # Loop the number of nodes - 1 times
   (num_nodes - 1).times do 
-
-    # Select a node which is not in added and has a the smallest distance value called current_node.
     current_node = find_next_node(added, shortest_distances)
 
+    # Check if graph is unconnected
     if current_node.nil?
       return answer
     end
@@ -45,16 +44,15 @@ def dijkstra(adjacency_matrix, start_node)
     # Set current_node in added to true
     added[current_node] = true
 
-    # Update all the values in distance for the neighbors of current_node with the minimum of 
-    # their current value and the distance to the current_node and the weight of the edge connecting them with current_node
+    # Update all the values in shortest_distances for the neighbors of current_node 
+    # with the minimum of their current value and the distance to the current_node and the weight of the edge connecting them with current_node
     adjacency_matrix[current_node].each_with_index do |distance, index|
-
       if distance > 0
         if parent_list[current_node]
           distance += shortest_distances[current_node]
         end
 
-        if shortest_distances[index] && (distance < shortest_distances[index])
+        if distance < shortest_distances[index]
           shortest_distances[index] = distance
           parent_list[index] = current_node
         end
